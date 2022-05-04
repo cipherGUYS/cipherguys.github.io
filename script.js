5ff8bf37631eab0bbf98b9ff0c85e265379efccf
Reenphy George <79626816+reenphygeorge@users.noreply.github.com>
2022-05-04 23:21:25 +0530

    Github Redirect

---
+++ script.js
@@ -0,0 +1,10 @@
+function redirect(str) {
+    switch (str) {
+        case 'gh':
+            window.location.href = "https://github.com/cipherGUYS";
+            break;
+    
+        default:
+            break;
+    }
+}
